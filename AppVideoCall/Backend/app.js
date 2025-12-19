e524485117229a9499d2a48dcea06982e4a5c7b2
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2025-12-19 18:41:08 +0530

    Starting Phase

--- AppVideoCall/Backend/app.js
+++ AppVideoCall/Backend/app.js
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import connectSocketIO from './src/controllers/socketManager.js';
 import userRoutes from './src/routes/userRoute.js';
-const app = express()
+const app = express();
 const server = createServer(app);
 const io = connectSocketIO(server);
 app.set("port",(process.env.PORT || 3000));
