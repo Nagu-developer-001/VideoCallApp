a5430fdee1f1e7192143476526aad3e2369bb73e
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2025-12-21 13:36:03 +0530

    Starting Phase

--- AppVideoCall/Backend/app.js
+++ AppVideoCall/Backend/app.js
@@ -8,7 +8,7 @@ import userRoutes from './src/routes/userRoute.js';
 const app = express();
 const server = createServer(app);
 const io = connectSocketIO(server);
-app.set("port",(process.env.PORT || 3000));
+app.set("port",(process.env.PORT || 3000))
 app.use(cors());
 app.use(express.json({limit:'40kb'}));
 app.use(express.urlencoded({ extended: true, limit:'40kb' }));
