6747ac115691e29e808192f73c6a475e47ca513f
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2025-12-23 07:15:26 +0530

    Starting Phase

--- AppVideoCall/Backend/app.js
+++ AppVideoCall/Backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 const server = createServer(app);
 const io = connectSocketIO(server);
 app.set("port",(process.env.PORT || 3000));
-app.use(cors());
+app.use(cors())
 app.use(express.json({limit:'40kb'}));
 app.use(express.urlencoded({ extended: true, limit:'40kb' }));
 app.use('/api/v1/users',userRoutes);
