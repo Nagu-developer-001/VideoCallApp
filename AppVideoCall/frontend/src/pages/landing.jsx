d66c5a03d9d059a3c88fc40970af070d653f7172
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2026-01-18 18:50:44 +0530

    Late Commit

---
+++ AppVideoCall/frontend/src/pages/landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Landing() {
+    return (
+    <div>
+        <h1>Welcome to AppVideoCall</h1>
+        <p>Your gateway to seamless video communication.</p>
+    </div>
+    );
+}
