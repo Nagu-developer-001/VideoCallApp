4b8f3a387085ba8bac0697ebe28c8c4b62566a17
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2026-01-20 06:54:16 +0530

    Late Commit

--- AppVideoCall/frontend/src/pages/landing.jsx
+++ AppVideoCall/frontend/src/pages/landing.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
     return (
     <div>
-        <h1>Welcome to AppVideoCall</h1>
-        <p>Your gateway to seamless video communication.</p>
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>VideoCallApp</h2>
+                </div>
+                <div className='navList'>
+                    <p>Join as Guest</p>
+                    <p>Register</p>
+                    <div role='button'><p>login</p></div>
+                </div>
+            </nav>
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{color:"#ff9"}}>Connect</span> with you</h1>
+                    <p>Cover a Distance by Apna Video Call</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Stared</Link>
+                    </div>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
     </div>
     );
 }
