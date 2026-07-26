42aade7cb5be475e80d1c9a94a5017b1022a6007
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2026-07-26 21:25:00 +0530

    Commit

--- AppVideoCall/frontend/src/pages/Authentication.jsx
+++ AppVideoCall/frontend/src/pages/Authentication.jsx
@@ -70,14 +70,14 @@ export default function Authentication() {
                     xs={false}
                     sm={4}
                     md={7}
-                    sx={{
-                        backgroundImage: 'url(https://unsplash.com/photos/person-in-red-and-black-plaid-long-sleeve-shirt-using-black-laptop-computer-HJmxky8Fvmo)',
-                        backgroundRepeat: 'no-repeat',
-                        backgroundColor: (t) =>
-                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                    }}
+                    // sx={{
+                    //     //backgroundImage: 'url(https://unsplash.com/photos/person-in-red-and-black-plaid-long-sleeve-shirt-using-black-laptop-computer-HJmxky8Fvmo)',
+                    //     //backgroundRepeat: 'no-repeat',
+                    //     backgroundColor: (t) =>
+                    //         t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    //     backgroundSize: 'cover',
+                    //     backgroundPosition: 'center',
+                    // }}
                 />
                 <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
                     <Box
